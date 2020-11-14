eb7267658d01af9bd6a993c23cdbc4c950a12d58
Rodrigo Sturm <rodrigoks@gmail.com>
2020-11-14 20:10:55 -0300

    implementando pipe data.

--- src/app/conversor/pipes/data-br.pipe.ts
+++ src/app/conversor/pipes/data-br.pipe.ts
@@ -5,8 +5,24 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DataBrPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
-  }
+  /**
+     * Aplica formatação para a data.
+     * 
+     * @param string dataEn
+     * @return string data no formato dd/mm/yyyy
+     */
+	transform(dataEn: string): string {
+    if (!dataEn) {
+           return '';
+       }
+   
+       const dataArr = dataEn.split('-');
+   
+       if (dataArr.length !== 3) {
+           return dataEn;
+       }
+       
+       return dataArr[2] + '/' + dataArr[1] + '/' + dataArr[0];
+ }
 
 }
